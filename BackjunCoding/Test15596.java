783c2e978a625fcc713a5c0c3836b95ffdea7707
superscman <103426932+superscman@users.noreply.github.com>
2022-09-19 18:33:06 +0900

    Add files via upload

--- BackjunCoding/Test15596.java
+++ BackjunCoding/Test15596.java
@@ -1,8 +1,6 @@
 package BackjunCoding;
 
-import java.util.Scanner;
-
-class Test {
+class Test15596 {
     public static long sum(int[] a) {
         long result = 0;
         for(int i =0; i<=a.length-1; i++) {
@@ -14,9 +12,3 @@ class Test {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        Test.sum(arr);
-    }
-}
