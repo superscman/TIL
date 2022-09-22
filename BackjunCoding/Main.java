2be23981d8b1125a2aaaa8f45923c47ce5e768f3
superscman <103426932+superscman@users.noreply.github.com>
2022-09-22 09:42:28 +0900

    백준 알고리즘

--- BackjunCoding/Main.java
+++ BackjunCoding/Main.java
@@ -1,24 +1,35 @@
 package BackjunCoding;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        //고정 비용 A만원, 한대 노트북 생산당 가변비용 B만원, 노트북 가격은 C만원
+        //총 수입(판매비용) > 총비용(고정비용+가변비용) 언젠간 많아짐.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       int a = in.nextInt();
+        int a = Integer.parseInt(st.nextToken()); //고정비용
+        int b = Integer.parseInt(st.nextToken()); // 가변 비용
+        int c = Integer.parseInt(st.nextToken()); // 노트북 가격
 
-       int b = in.nextInt();
-
-       int s1 = (a/100)+((a/10)*10)+((a%10)*100);
-
-       int s2 = (b/100)+((b/10)*10)+((b%10)*100);
-
-       if(s1 < s2) System.out.println(s2);
-       if(s2 < s1) System.out.println(s1);
-
+        long count = 0; // 판매 대수
+        long ce = c; // 판매된 총 매출
+        long be = b; // 판매 될때 사용한 총 가변비용
+        if(b>=c) System.out.println(-1); // 가변비용이 매출보다 크면 -1
+        else if(a + b >= c) {
+            while (a + be >= ce) {
+                ++count;
+                ce = c * count;
+                be = b * count;
+            }
+            System.out.println(count);
+        }else if(a + b < c && c >0) {
+            System.out.println(1);
+        }else System.out.println(-1);
     }
-
 }
