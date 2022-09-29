899d788c697181147c8bdb8e22180f3b249e1f3d
superscman <103426932+superscman@users.noreply.github.com>
2022-09-29 17:12:49 +0900

    Delete codeup1.java

--- codeup1.java
+++
@@ -1,8 +0,0 @@
-public class codeup1 {
-    public static void main(String[] args) {
-        char a = (char) b; // 이거 약간 변수화 시켜주는 선언,할당같음.
-        str.charAt(index); //이건 진짜 꿀팁임. char 인덱스에 있는거 뽑아줌.
-        System.out.printf("%d", a<<1); //a*2의1제곱임. 어마어마한거임. a<<2 2의 4제곱. a>>1 반띵 와우
-        //아까 적어놓은것들 많았는데 저장하기 전에 날아가부렀어요 ㅠㅠㅠ 내일은 잘적어놔서 올릴게요.
-    }
-}
